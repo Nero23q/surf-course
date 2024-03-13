608208f418bf9ab64cf96ba0f56099a1f3f895cc
Nero23q <160020289+Nero23q@users.noreply.github.com>
2024-03-13 21:50:21 +0400

    Add files via upload

---
+++ task-02.dart
@@ -0,0 +1,183 @@
+enum Countries { brazil, russia, turkish, spain, japan }
+
+class Territory {
+  final int areaInHectare;
+  final List<String> crops;
+  final List<AgriculturalMachinery> machineries;
+
+  Territory(
+    this.areaInHectare,
+    this.crops,
+    this.machineries,
+  );
+}
+
+class AgriculturalMachinery {
+  final String id;
+  final DateTime releaseDate;
+
+  AgriculturalMachinery(
+    this.id,
+    this.releaseDate,
+  );
+
+  /// Переопределяем оператор "==", чтобы сравнивать объекты по значению.
+  @override
+  bool operator ==(Object? other) {
+    if (other is! AgriculturalMachinery) return false;
+    if (other.id == id && other.releaseDate == releaseDate) return true;
+
+    return false;
+  }
+
+  @override
+  int get hashCode => id.hashCode ^ releaseDate.hashCode;
+}
+
+final mapBefore2010 = <Countries, List<Territory>>{
+  Countries.brazil: [
+    Territory(
+      34,
+      ['Кукуруза'],
+      [
+        AgriculturalMachinery(
+          'Трактор Степан',
+          DateTime(2001),
+        ),
+        AgriculturalMachinery(
+          'Культиватор Сережа',
+          DateTime(2007),
+        ),
+      ],
+    ),
+  ],
+  Countries.russia: [
+    Territory(
+      14,
+      ['Картофель'],
+      [
+        AgriculturalMachinery(
+          'Трактор Гена',
+          DateTime(1993),
+        ),
+        AgriculturalMachinery(
+          'Гранулятор Антон',
+          DateTime(2009),
+        ),
+      ],
+    ),
+    Territory(
+      19,
+      ['Лук'],
+      [
+        AgriculturalMachinery(
+          'Трактор Гена',
+          DateTime(1993),
+        ),
+        AgriculturalMachinery(
+          'Дробилка Маша',
+          DateTime(1990),
+        ),
+      ],
+    ),
+  ],
+  Countries.turkish: [
+    Territory(
+      43,
+      ['Хмель'],
+      [
+        AgriculturalMachinery(
+          'Комбаин Василий',
+          DateTime(1998),
+        ),
+        AgriculturalMachinery(
+          'Сепаратор Марк',
+          DateTime(2005),
+        ),
+      ],
+    ),
+  ],
+};
+
+final mapAfter2010 = {
+  Countries.turkish: [
+    Territory(
+      22,
+      ['Чай'],
+      [
+        AgriculturalMachinery(
+          'Каток Кирилл',
+          DateTime(2018),
+        ),
+        AgriculturalMachinery(
+          'Комбаин Василий',
+          DateTime(1998),
+        ),
+      ],
+    ),
+  ],
+  Countries.japan: [
+    Territory(
+      3,
+      ['Рис'],
+      [
+        AgriculturalMachinery(
+          'Гидравлический молот Лена',
+          DateTime(2014),
+        ),
+      ],
+    ),
+  ],
+  Countries.spain: [
+    Territory(
+      29,
+      ['Арбузы'],
+      [
+        AgriculturalMachinery(
+          'Мини-погрузчик Максим',
+          DateTime(2011),
+        ),
+      ],
+    ),
+    Territory(
+      11,
+      ['Табак'],
+      [
+        AgriculturalMachinery(
+          'Окучник Саша',
+          DateTime(2010),
+        ),
+      ],
+    ),
+  ],
+};
+
+void main() {
+  List<Territory> allTerritories = [
+    ...mapBefore2010.values,
+    ...mapAfter2010.values
+  ].expand((element) => element).toList();
+
+  Set<AgriculturalMachinery> allMachinerys = allTerritories
+      .map((e) => e.machineries)
+      .expand((element) => element)
+      .toSet();
+
+  final List<int> releaseDate =
+      allMachinerys.map((e) => e.releaseDate.year).toList();
+  final currentYear = DateTime.now().year;
+  final totalAge = releaseDate
+      .map((year) => currentYear - year)
+      .reduce((value, element) => value + element);
+  final averageYear = (totalAge / releaseDate.length).round();
+  print("Средний возраст всей техники = $averageYear");
+
+  final List<int> sortedDate = releaseDate..sort();
+  final List<int> halfYear =
+      sortedDate.take((sortedDate.length / 2).round()).toList();
+  final totalOlderAge = halfYear
+      .map((year) => currentYear - year)
+      .reduce((value, element) => value + element);
+  final averageOlderYear = (totalOlderAge / halfYear.length).round();
+  print("Средний возраст старой техники = $averageOlderYear");
+}
